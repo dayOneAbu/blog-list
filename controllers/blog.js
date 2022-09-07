01dfbb9c4e1d1eaf9b25d2472bd3b713711e4f5c
Abubker M <abubker.init@gmail.com>
2022-09-07 20:17:21 +0300

    Blog list, step2: refactoring the app

---
+++ controllers/blog.js
@@ -0,0 +1,22 @@
+const blogRouter = require("express").Router();
+const Blog = require("../models/Blog");
+blogRouter.get("/", (req, res) => {
+  Blog.find({}).then((blogs) => {
+    res.json(blogs);
+  });
+});
+
+blogRouter.post("/", (req, res) => {
+  const { title, author, url, likes } = req.body;
+  const blog = new Blog({
+    title,
+    author,
+    url,
+    likes,
+  });
+
+  blog.save().then((result) => {
+    res.status(201).json(result);
+  });
+});
+module.exports = blogRouter;
---
+++ index.js
@@ -0,0 +1,9 @@
+const http = require("http");
+const app = require("./app");
+const server = http.createServer(app);
+const { PORT } = require("./utils/config");
+const logger = require("./utils/logger");
+
+server.listen(PORT, () => {
+  logger.info(`Server running on port ${PORT}`);
+});
